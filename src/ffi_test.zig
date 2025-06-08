7cc092b4d3b859a78f5f80de127be68209bd0864
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-06-08 03:07:31 +0300

    invoke all test ids for ffi_test

--- src/ffi_test.zig
+++ src/ffi_test.zig
@@ -33,7 +33,8 @@ fn make_array(id: u8, allocator: Allocator) !arr.Array {
         15 => .{ .large_utf8 = try builder.LargeUtf8Builder.from_slice_opt(&.{ "hello", "world", null }, allocator) },
         16 => .{ .decimal128 = try builder.Decimal128Builder.from_slice(.{ .precision = 31, .scale = -31 }, &.{ 1, 2, 3, 4, 69 }, true, allocator) },
         17 => .{ .decimal256 = try builder.Decimal256Builder.from_slice(.{ .precision = 31, .scale = -31 }, &.{ 69, -69 }, false, allocator) },
-        // 18 => .{ .date32 = try builder.Date32Builder.from }
+        18 => .{ .date32 = try builder.Date32Builder.from_slice(&.{ 69, 69, 11, 15 }, false, allocator) },
+        19 => .{ .date64 = try builder.Date64Builder.from_slice(&.{ 69, 69, 11, 15 }, false, allocator) },
         else => unreachable,
     };
 }
@@ -60,6 +61,13 @@ fn run_test(id: u8) !void {
     try equals(&output_array, &input_array);
 }
 
+// fn run_test(id: u8) !void {
+//     return run_test_impl(id) catch |e| err: {
+//         std.log.err("failed test id: {}", .{id});
+//         break :err e;
+//     };
+// }
+
 test "ffi basic" {
     try run_test(0);
     try run_test(1);
@@ -79,4 +87,27 @@ test "ffi basic" {
     try run_test(15);
     try run_test(16);
     try run_test(17);
+    try run_test(18);
+    try run_test(19);
+    try run_test(20);
+    try run_test(21);
+    try run_test(22);
+    try run_test(23);
+    try run_test(24);
+    try run_test(25);
+    try run_test(26);
+    try run_test(27);
+    try run_test(28);
+    try run_test(29);
+    try run_test(30);
+    try run_test(31);
+    try run_test(32);
+    try run_test(33);
+    try run_test(34);
+    try run_test(35);
+    try run_test(36);
+    try run_test(37);
+    try run_test(38);
+    try run_test(39);
+    try run_test(40);
 }
